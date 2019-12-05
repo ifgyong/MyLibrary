d25734a92f6a99a509a92b3d25a2509a4ca76142
fanguangyong <fanguangyong@xqafu.com>
2019-12-05 16:26:29 +0800

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,21 @@
+struct MyLibrary {
+    var text = "Hello, World!"
+}
+
+
+
+class Person {
+	let name = ""
+	let age = 0
+	
+	
+	init() {
+		
+	}
+	func say() -> Void {
+		print("name:\(name) age:\(age)")
+	}
+	func run() -> Void {
+		print("name:\(name) is runing")
+	}
+}
